b917ee85b0f2dcd052e97a13ae7334af74fa28a5
SpongeBed81 <cmodz7969@gmail.com>
2023-02-11 22:37:51 +0300

    fixes

--- src/components/sidebar.svelte
+++ src/components/sidebar.svelte
@@ -1,6 +1,7 @@
 <script>
  import { page } from "$app/stores"
  import { onMount } from "svelte";
+ import { navigating } from '$app/stores';
  let root;
  let open = false;
  let icons = [
@@ -9,6 +10,11 @@
   //açılmış
   `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`
  ]
+
+ $: if($navigating) {
+  new Audio("/switch-page.mp3").play();
+  selectRoute($navigating.to.route.id)
+ }
 
  function selectRoute(route) {
    const getTop = root.querySelector(`[href="${route}"]`).getBoundingClientRect()
@@ -20,9 +26,7 @@
   selectRoute(window.location.pathname)
   for (let route of root.querySelector(".routes").children) {
    route.addEventListener("click", () => {
-    selectRoute(route.getAttribute("href"))
     controlNavbar()
-    new Audio("/switch-page.mp3").play();
    })
   }
  })
