37eab3ea4047c79076242eedb176254d977964a1
SpongeBed81 <cmodz7969@gmail.com>
2023-08-18 19:04:50 +0300

    added go back button

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -3,6 +3,7 @@
   import CopyButton from "../../../components/copy-button.svelte";
   import "../../../markdown-theme.css";
   import "../../../prism.css";
+  import { goto } from "$app/navigation";
   export let data;
 
   $: component = data.component;
@@ -37,7 +38,21 @@
 
 <Scene>
   <div class="metadata-body">
-    <h1>{metadata.title}</h1>
+    <div class="flex-title">
+      <button on:click={() => goto("/blog")}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          ><path
+            fill="currentColor"
+            d="m7.825 13l5.6 5.6L12 20l-8-8l8-8l1.425 1.4l-5.6 5.6H20v2H7.825Z"
+          /></svg
+        >
+      </button>
+      <h1>{metadata.title}</h1>
+    </div>
     <h3>{metadata.description}</h3>
     <div class="published-by">
       <img
@@ -62,6 +77,30 @@
 </Scene>
 
 <style>
+  .flex-title {
+    display: flex;
+    gap: 12px;
+    align-items: center;
+  }
+
+  .flex-title > button {
+    border-radius: 4px;
+    background-color: transparent;
+    border: none;
+    height: fit-content;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 0 0 1px var(--color-border-default);
+    transition: box-shadow 0.2s ease;
+    cursor: pointer;
+    padding: 4px;
+  }
+
+  .flex-title > button:hover {
+    box-shadow: 0 0 0 2px white;
+  }
+
   .published-by {
     display: flex;
     align-items: center;
